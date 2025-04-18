9bc411cf55019176393783ed6249689b7023aea6
Kaushal <kaushal14707@gmail.com>
2025-04-18 17:51:14 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -47,6 +47,14 @@ router.post('/signup', async (req, res) => {
 });
 
 // Login route
+router.options('/login', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send();
+});
+
 router.post('/login', async (req, res) => {
   try {
     // const { email, password } = req.body; // Removed
